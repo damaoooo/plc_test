a09fe6c1cf318871e4ba47a1b7b33b8ecd2b047b
damaoooo <damaoooo@163.com>
2023-08-16 01:39:26 +0300

    114514

--- exp.py
+++ exp.py
@@ -6,5 +6,5 @@ import pickle
 
 if __name__ == '__main__':
     file_tree = FileScanner(root_path='coreutil_dataset').scan()
-    converter = Converter(op_file='coreutil_dataset/op_file.pkl', read_op=True)
-    DataGenerator(file_tree=file_tree, save_path='./dataset/new_core', converter=converter).run()
+    converter = Converter(op_file='coreutil_dataset/op_file.pkl', read_op=False)
+    DataGenerator(file_tree=file_tree, save_path='./dataset/new_core', converter=converter, read_cache=True).run()
