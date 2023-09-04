dd17744707cbd5edd8d419ecfbcde3d593528819
damaoooo <damaoooo@163.com>
2023-09-04 23:09:24 +0300

    add bf16 benchmark

--- function.py
+++ function.py
@@ -37,6 +37,9 @@ with torch.no_grad():
         print("CUDA FP16 time: ", (end_time - start_time) / 10)
         
 
+dummy_graph = dgl.graph(([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), num_nodes=500)
+dummy_graph = dgl.add_self_loop(dummy_graph)
+dummy_graph.ndata['feat'] = torch.randn(500, 151)
 print("CPU FP32 testing... ")
 model = MyModel(151, 128, 64, 6, 0.3, 0.2, 500)
 model.eval()
@@ -48,3 +51,20 @@ with torch.no_grad():
     end_time = time.time()
     print("CPU FP32 time: ", (end_time - start_time) / 10)
     
+print("CPU BF16 testing... ")
+dummy_graph = dgl.graph(([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), num_nodes=500)
+dummy_graph = dgl.add_self_loop(dummy_graph)
+dummy_graph.ndata['feat'] = torch.randn(500, 151, dtype=torch.bfloat16)
+dummy_graph = dgl.to_bfloat16(dummy_graph)
+
+
+model = MyModel(151, 128, 64, 6, 0.3, 0.2, 500)
+model = model.to(dtype=torch.bfloat16)
+model.eval()
+
+with torch.no_grad():
+    start_time = time.time()
+    for i in range(1000):
+        s = model(dummy_graph)
+    end_time = time.time()
+    print("CPU FP16 time: ", (end_time - start_time) / 10)
