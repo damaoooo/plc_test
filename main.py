23ce149aa34c9998eafbfc8406ecc0223f5fa931
damaoooo <damaoooo@163.com>
2024-07-08 01:14:35 +0300

    waiting for debug

--- main.py
+++ main.py
@@ -104,8 +104,21 @@ def load_model_checkpoint(model: ReGraphModel, path: str):
     return model
 
 
+def seed_everything(seed: int):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
 if __name__ == "__main__":
     torch.set_float32_matmul_precision('medium')
 
     config: TrainConfig = read_config()
     random_seed = config.seed
+
+    seed_everything(random_seed)
+    train(config)
+
